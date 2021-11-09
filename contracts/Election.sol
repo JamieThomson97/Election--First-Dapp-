31c4b2441eb487b7100974ebe7f81948e9ed9cf5
Jamie Thomson <jaytadmn@gmail.com>
2021-11-09 22:16:58 +0000

    create election with 3 candidate with passing tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,25 @@
 pragma solidity >=0.4.22 <=0.8.10;
 
 contract Election {
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    function addCandidate(string memory name) private {
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount, name, 0);
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+    uint256 public candidateCount;
+
     string public candidate;
 
     constructor() public {
-        candidate = "candidate 1";
+        addCandidate("Jamie");
+        addCandidate("James");
+        addCandidate("Philip");
     }
 }
